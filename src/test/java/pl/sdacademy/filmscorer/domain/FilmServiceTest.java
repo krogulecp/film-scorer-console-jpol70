37ea65d574ccbe4d0e71019aebd959748e403aac
Piotr Krogulec <piotr.krogulec@allegro.pl>
2021-04-25 12:37:16 +0200

    part 2 (incomplete)

--- src/test/java/pl/sdacademy/filmscorer/domain/FilmServiceTest.java
+++ src/test/java/pl/sdacademy/filmscorer/domain/FilmServiceTest.java
@@ -1,16 +1,19 @@
 package pl.sdacademy.filmscorer.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class FilmServiceTest {
 
     private FilmService filmService;
+    private FilmRepository filmRepository;
 
     @BeforeEach
     void setup(){
         filmService = new FilmService(filmRepository);
     }
 
+    @Test
     void shouldAddFilm() {
         //given
         final String title = "Rambo";
