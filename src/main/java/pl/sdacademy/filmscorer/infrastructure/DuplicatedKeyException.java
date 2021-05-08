410d7f258d69ba6cf73b5e15d1f019eb5ae17061
Piotr Krogulec <piotr.krogulec@allegro.pl>
2021-05-08 10:28:35 +0200

    added in memory implementation

--- src/main/java/pl/sdacademy/filmscorer/infrastructure/DuplicatedKeyException.java
+++ src/main/java/pl/sdacademy/filmscorer/infrastructure/DuplicatedKeyException.java
@@ -2,7 +2,7 @@ package pl.sdacademy.filmscorer.infrastructure;
 
 public class DuplicatedKeyException extends RuntimeException {
 
-    public DuplicatedKeyException(String message, Throwable cause) {
-        super(message, cause);
+    public DuplicatedKeyException(String message) {
+        super(message);
     }
 }
--- src/main/java/pl/sdacademy/filmscorer/infrastructure/InMemoryFilmRepository.java
+++ src/main/java/pl/sdacademy/filmscorer/infrastructure/InMemoryFilmRepository.java
@@ -3,9 +3,25 @@ package pl.sdacademy.filmscorer.infrastructure;
 import pl.sdacademy.filmscorer.domain.Film;
 import pl.sdacademy.filmscorer.domain.FilmRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryFilmRepository implements FilmRepository {
+
+    private static final Map<String, Film> films = new HashMap<>();
+
     @Override
     public void saveOrThrowIfExists(Film film) {
-        throw new RuntimeException("Not implemented yet!!!");
+        final String key = constructKey(film);
+        final Film foundFilm = films.get(key);
+        if (foundFilm == null) {
+            films.put(key, film);
+        } else {
+            throw new DuplicatedKeyException("Film with title " + film.getTitle() + " released in " + film.getReleaseYear() + " already exists!");
+        }
+    }
+
+    private String constructKey(Film film) {
+        return film.getTitle() + film.getReleaseYear();
     }
 }
